ba9f3d3adf0a79c4f2593e3678ade4f749640b8f
serge <agentlukas321@gmail.com>
2022-04-12 19:53:07 +0300

    Created a class Rounder

--- src/app/MerchantA.java
+++ src/app/MerchantA.java
@@ -15,5 +15,17 @@ public class MerchantA extends Merchant implements Bonus{
         return "Продавец: " + name + ", " + phone + ", " + email + "\n";
     }
 
+    @Override
+    public double calcBonus(double sales) {
+        double bonus;
+        if (sales <= 10000) {
+            bonus = sales * 5 / 100;
+        } else if (sales > 10000 && sales <= 30000) {
+            bonus = sales * 10 / 100;
+        } else {
+            bonus = sales * 15 / 100;
+        }
+        return bonus;
+    }
 
 }
