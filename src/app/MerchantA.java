bab90e7dffd679f86feb5e2d20445da1565a62f0
serge <agentlukas321@gmail.com>
2022-04-12 19:46:56 +0300

    Add method infoMerchant: return string about merchant

--- src/app/MerchantA.java
+++ src/app/MerchantA.java
@@ -1,4 +1,17 @@
 package app;
 
 public class MerchantA extends Merchant {
+
+    String email;
+
+    public MerchantA(String name, String phone, String email) {
+        super(name, phone);
+        this.email = email;
+    }
+
+    public String infoMerchant() {
+        String name = getName();
+        String phone = getPhone();
+        return "Продавец: " + name + ", " + phone + ", " + email + "\n";
+    }
 }
