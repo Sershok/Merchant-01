5f832761d3538920479e3e2f22e91ef3945009ca
serge <agentlukas321@gmail.com>
2022-04-12 19:41:51 +0300

    Created a class MerchantA: extends Merchant

--- src/app/Merchant.java
+++ src/app/Merchant.java
@@ -1,5 +1,19 @@
 package app;
 
 public class Merchant {
+    private final String name;
+    private final String phone;
 
+    public Merchant(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
