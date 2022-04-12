8791b302c9fa4dd09ae75e281109b20c60394b6c
serge <agentlukas321@gmail.com>
2022-04-12 20:05:53 +0300

    Update class App: add vars

--- src/app/app.java
+++ src/app/app.java
@@ -1,6 +1,19 @@
 package app;
 
 public class app {
+
+    static String merchantName;
+    static String email;
+    static String phone;
+    static String productName;
+    static int quantity;
+    static double price;
+    static String roundBonus;
+    static ProductA product;
+    static MerchantA merchantA;
+    static String infoMerchant;
+    static String infoProduct;
+
     public static void main(String[] args) {
 
     }
